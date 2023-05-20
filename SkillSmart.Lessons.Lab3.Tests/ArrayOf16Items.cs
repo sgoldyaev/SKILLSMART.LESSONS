c7c69c2a37394f868c492302b70d1a6ebc07be33
Sergey Goldyaev <sgoldyaev@gmail.com>
2023-05-20 10:34:58 +0300

    Add 32 items array unit tests

--- SkillSmart.Lessons.Lab3.Tests/ArrayOf16Items.cs
+++ SkillSmart.Lessons.Lab3.Tests/ArrayOf16Items.cs
@@ -2,11 +2,11 @@
 
 public static class ArrayOf16Items
 {
-    public static readonly int[] Full             = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    public static readonly int[] FirstOneIsMissed = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0};
-    public static readonly int[] FirstTwoIsMissed = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0, 0};
-    public static readonly int[] LastOneIsMissed  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
-    public static readonly int[] LastTwoIsMissed  = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 0};
-    public static readonly int[] MiddleOneIsMissed  = {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0};
-    public static readonly int[] MiddleTwoIsMissed  = {1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0, 0};
+    public static readonly int[] Full              = {01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16};
+    public static readonly int[] FirstOneIsMissed  = {02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 00};
+    public static readonly int[] FirstTwoIsMissed  = {03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 00, 00};
+    public static readonly int[] LastOneIsMissed   = {01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 00};
+    public static readonly int[] LastTwoIsMissed   = {01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 00, 00};
+    public static readonly int[] MiddleOneIsMissed = {01, 02, 03, 04, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 00};
+    public static readonly int[] MiddleTwoIsMissed = {01, 02, 03, 04, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 00, 00};
 }
--- SkillSmart.Lessons.Lab3.Tests/DynArrayRemoveTests.cs
+++ SkillSmart.Lessons.Lab3.Tests/DynArrayRemoveTests.cs
@@ -14,6 +14,7 @@ namespace SkillSmart.Lessons.Lab3.Tests
             dinArray = new DynArray<int>();
         }
 
+        #region 16 items array
         [TestMethod]
         public void RemoveFirstOneItemFrom16ItemsArrayTest()
         {
@@ -100,5 +101,95 @@ namespace SkillSmart.Lessons.Lab3.Tests
             Assert.AreEqual(DynArray<int>.defaultSize, dinArray.array.Length);
             CollectionAssert.AreEqual(ArrayOf16Items.MiddleTwoIsMissed, dinArray.array);
         }
+        #endregion
+        
+        #region 32 items array
+        [TestMethod]
+        public void RemoveFirstOneItemFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(0);
+            
+            Assert.AreEqual(31, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.FirstOneIsMissed, dinArray.array);
+        }
+
+        [TestMethod]
+        public void RemoveFirstTwoItemFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(0);
+            dinArray.Remove(0);
+            
+            Assert.AreEqual(30, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.FirstTwoIsMissed, dinArray.array);
+        }
+
+        [TestMethod]
+        public void RemoveLastOneItemFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(31);
+            
+            Assert.AreEqual(31, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.LastOneIsMissed, dinArray.array);
+        }
+
+        [TestMethod]
+        public void RemoveLastTwoItemFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(31);
+            dinArray.Remove(30);
+            
+            Assert.AreEqual(30, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.LastTwoIsMissed, dinArray.array);
+        }
+
+        [TestMethod]
+        public void RemoveMiddleOneItemFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(4);
+            
+            Assert.AreEqual(31, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.MiddleOneIsMissed, dinArray.array);
+        }
+
+        [TestMethod]
+        public void RemoveMiddleTwoItemsFrom32ItemsArrayTest()
+        {
+            foreach (var item in ArrayOf32Items.Full)
+                dinArray.Append(item);
+
+            dinArray.Remove(4);
+            dinArray.Remove(4);
+            
+            Assert.AreEqual(30, dinArray.count);
+            Assert.AreEqual(32, dinArray.capacity);
+            Assert.AreEqual(32, dinArray.array.Length);
+            CollectionAssert.AreEqual(ArrayOf32Items.MiddleTwoIsMissed, dinArray.array);
+        }
+        #endregion
    }
 }
