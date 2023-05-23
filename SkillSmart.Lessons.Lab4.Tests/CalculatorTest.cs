bdbffb758a5578a75a9a0f5c3731b67937d4e27d
Sergey Goldyaev <sgoldyaev@gmail.com>
2023-05-23 20:32:22 +0300

    Add Lab4 bonus

---
+++ SkillSmart.Lessons.Lab4.Tests/CalculatorTest.cs
@@ -0,0 +1,27 @@
+using AlgorithmsDataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkillSmart.Lessons.Lab3.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Init() 
+        {
+            calculator = new Calculator();
+        }
+
+        [TestMethod]
+        [DataRow("1 2 + 3 * =", 9)]
+        [DataRow("8 2 + 5 * 9 + =", 59)]
+        public void PushSingleElementTest(string expression, int expectedResult)
+        {
+            var actualResult = calculator.Calculate(expression);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ SkillSmart.Lessons.Lab4/Calculator.cs
@@ -0,0 +1,46 @@
+﻿namespace AlgorithmsDataStructures;
+
+public class Calculator
+{
+    private static readonly Dictionary<string, Func<int, int, int>> operations =
+        new Dictionary<string, Func<int, int, int>>()
+        {
+            { "+", (left, right) => left + right },
+            { "-", (left, right) => left - right },
+            { "*", (left, right) => left * right },
+            { "/", (left, right) => left / right },
+        };
+
+    public int Calculate(string postFixExpression)
+    {
+        var s1 = new Stack<string>();
+        var s2 = new Stack<int>();
+
+        var expression = postFixExpression.Split(' ');
+        
+        for (var index = expression.Length - 1; index >= 0; index--)
+            s1.Push(expression[index]);
+
+        while (s1.Size() > 0)
+        {
+            var element = s1.Pop();
+
+            if (int.TryParse(element, out var e))
+                s2.Push(e);
+            
+            else if (operations.TryGetValue(element, out var operation))
+            {
+                var right = s2.Pop();
+                var left = s2.Pop();
+                var result = operation(left, right);
+                
+                s2.Push(result);
+            }
+            
+            else if (element == "=")
+                return s2.Pop();
+        }
+
+        return default;
+    }
+}
