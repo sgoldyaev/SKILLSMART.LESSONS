6506b24cd81d6d3c9dbd42f16a3d6a663dc4a675
Sergey Goldyaev <sgoldyaev@gmail.com>
2023-06-01 22:10:43 +0300

    Add QueueOnStacks to Lab5

---
+++ SkillSmart.Lessons.Lab5/QueueOnStacks.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class QueueOnStacks<T>
+    {
+        private readonly Stack<T> s1;
+        private readonly Stack<T> s2;
+
+        public QueueOnStacks()
+        {
+            s1 = new Stack<T>();
+            s2 = new Stack<T>();
+        } 
+
+        public void Enqueue(T item)
+        {
+            while (s1.Count > 0)
+            {
+                s2.Push(s1.Pop());
+            }
+            
+            s1.Push(item);
+          
+            while (s2.Count > 0)
+            {
+                s1.Push(s2.Pop());
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (s1.Count > 0)
+            {
+                return s1.Pop();
+            }
+
+            return default(T);
+        }
+        
+        public int Size()
+        {
+            return s1.Count;
+        }
+        
+    }
+}
