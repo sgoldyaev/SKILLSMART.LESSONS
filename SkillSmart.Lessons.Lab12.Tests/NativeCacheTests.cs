b043f6fcfb7665b45d61d72d990af5c2651a808d
Sergey Goldyaev <sgoldyaev@gmail.com>
2023-06-14 16:11:05 +0300

    Lab12: Cleanup tests

--- SkillSmart.Lessons.Lab12.Tests/NativeCacheTests.cs
+++ SkillSmart.Lessons.Lab12.Tests/NativeCacheTests.cs
@@ -17,16 +17,6 @@ namespace SkillSmart.Lessons.Lab12.Tests
         }
 
         [TestMethod]
-        //[DataRow("0123456789")]
-        //[DataRow("1234567890")]
-        //[DataRow("2345678901")]
-        //[DataRow("3456789012")]
-        //[DataRow("4567890123")]
-        //[DataRow("5678901234")]
-        //[DataRow("6789012345")]
-        //[DataRow("7890123456")]
-        //[DataRow("8901234567")]
-        //[DataRow("9012345678")]
         public void GetEmptyCacheTests()
         {
             var actual = cache.Get("key");
